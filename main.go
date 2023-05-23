36d583f13b861ab8297098fa4e64ec7a2e964393
Jason Gardner <buhrietoe@gmail.com>
2023-05-23 13:40:43 -0500

    iterate platforms and devices

--- main.go
+++ main.go
@@ -11,44 +11,31 @@ import (
 )
 
 func main() {
-	platforms, err := cl.GetPlatforms()
-	if err != nil {
-		fmt.Println(err)
-	}
+	platforms, _ := cl.GetPlatforms()
 
 	if len(platforms) == 0 {
-		fmt.Println("== Found 0 OpenCL platforms ==")
+		fmt.Printf("== Found %d OpenCL platforms ==\n", len(platforms))
 		os.Exit(1)
 	}
 
-	devices, err := platforms[0].GetDevices(cl.DeviceTypeAll)
-	if err != nil {
-		fmt.Println(err)
-	}
+	for platformNum, platform := range platforms {
+		devices, _ := platform.GetDevices(cl.DeviceTypeAll)
 
-	if len(devices) == 0 {
-		fmt.Println("== Found 0 OpenCL devices ==")
-		os.Exit(1)
-	} else if len(devices) == 1 {
-		fmt.Println("== Found 1 OpenCL device ==")
-	} else {
-		fmt.Printf("== Found %d OpenCL devices ==\n", len(devices))
-	}
-
-	for _, device := range devices {
-		table := tablewriter.CreateTable()
-		table.UTF8Box()
-		table.AddTitle(device.Name())
-		table.AddRow("Device Type", device.Type())
-		table.AddRow("Supported OpenCL Version", device.OpenCLCVersion())
-		table.AddRow("Profile", device.Profile())
-		table.AddRow("Driver Version", device.DriverVersion())
-		table.AddRow("Number of Compute Units", strconv.Itoa(device.MaxComputeUnits()))
-		table.AddRow("Clock Frequency", strconv.Itoa(device.MaxClockFrequency())+" Mhz")
-		table.AddRow("Global Mem Size", ByteCountIEC(device.GlobalMemSize()))
-		table.AddRow("Allocatable Mem Size", ByteCountIEC(device.MaxMemAllocSize()))
-		table.AddRow("Is Host Unified Memory", device.HostUnifiedMemory())
-		fmt.Println("\n" + table.Render())
+		for deviceNum, device := range devices {
+			table := tablewriter.CreateTable()
+			table.UTF8Box()
+			table.AddTitle(strconv.Itoa(platformNum) + ": " + platform.Name() + " - " + strconv.Itoa(deviceNum) + ": " + device.Name())
+			table.AddRow("Device Type", device.Type())
+			table.AddRow("Supported OpenCL Version", device.OpenCLCVersion())
+			table.AddRow("Profile", device.Profile())
+			table.AddRow("Driver Version", device.DriverVersion())
+			table.AddRow("Number of Compute Units", strconv.Itoa(device.MaxComputeUnits()))
+			table.AddRow("Clock Frequency", strconv.Itoa(device.MaxClockFrequency())+" Mhz")
+			table.AddRow("Global Mem Size", ByteCountIEC(device.GlobalMemSize()))
+			table.AddRow("Allocatable Mem Size", ByteCountIEC(device.MaxMemAllocSize()))
+			table.AddRow("Is Host Unified Memory", device.HostUnifiedMemory())
+			fmt.Println("\n" + table.Render())
+		}
 	}
 }
 
